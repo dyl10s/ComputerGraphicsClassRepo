92e87b7407210cc792a54d6107e6d023c304feff
Dylan Strohschein <dylanstrohschein@gmail.com>
2021-03-28 23:47:09 -0400

    I have done it

--- labs/lab4/3d/index.js
+++ labs/lab4/3d/index.js
@@ -1,5 +1,58 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import { GLTFLoader } from 'https://unpkg.com/three@0.126.1/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/FlyControls.js';
+
+let characterModel;
+let stageModel;
+let videoLoaded = false;
+let backgroundLoaded = false;
+
+let cameraMove = {x: 0, y:0};
+
+let mixer;
+
+const video = document.getElementById('video');
+video.src = "./models/video.mp4"
+video.load();
+
+video.oncanplay = function() {
+    videoLoaded = true;
+}
+
+const backgroundTexture = new THREE.CubeTextureLoader().load([
+    './models/posx.jpg',
+    './models/negx.jpg',
+    './models/posy.jpg',
+    './models/negy.jpg',
+    './models/posz.jpg',
+    './models/negz.jpg',
+    ], () => { backgroundLoaded = true });
+
+var loader = new GLTFLoader();
+
+loader.load('./models/BellyDance.glb', function (model) {
+    mixer = new THREE.AnimationMixer( model.scene );
+    var action = mixer.clipAction( model.animations[ 0 ] );
+    action.play();
+
+    model.scene.position.z = 150;
+    characterModel = model.scene;
+});
+
+loader.load('./models/Stage.glb', function (model) {
+    model.scene.position.y = -50;
+    model.scene.scale.set(10,10,10);
+    stageModel = model.scene;
+});
+
+var waitForLoad = setInterval(() => {
+    if(characterModel && stageModel && videoLoaded && backgroundLoaded) {
+        document.getElementById("loadingText").remove();
+        document.getElementById("theButton").style.display = "block";
+        document.getElementById("warningText").style.display = "block";
+        clearInterval(waitForLoad);
+    }
+}, 50);
 
 document.getElementById("theButton").onclick = function(){
     InitiateMeme();
@@ -11,7 +64,6 @@ function InitiateMeme() {
     const scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    var mixer;
     var clock = new THREE.Clock();
 
     const renderer = new THREE.WebGLRenderer();
@@ -23,48 +75,11 @@ function InitiateMeme() {
         camera.aspect = window.innerWidth / window.innerHeight;
     }
 
-    var loader = new GLTFLoader();
-
-    let characterModel;
-
-    loader.load('./models/BellyDance.glb', function (model) {
-
-        mixer = new THREE.AnimationMixer( model.scene );
-        var action = mixer.clipAction( model.animations[ 0 ] );
-        action.play();
-
-        model.scene.position.z = 150;
-
-        scene.add(model.scene);
-        characterModel = model.scene;
-    });
-
-    let stageModel;
-
-    loader.load('./models/Stage.glb', function (model) {
-        model.scene.position.y = -50;
-        model.scene.scale.set(10,10,10);
-        scene.add(model.scene);
-        stageModel = model.scene;
-    });
-
     // Setup skybox
-    const backgroundTexture = new THREE.CubeTextureLoader().load([
-    './models/posx.jpg',
-    './models/negx.jpg',
-    './models/posy.jpg',
-    './models/negy.jpg',
-    './models/posz.jpg',
-    './models/negz.jpg',
-    ]);
-
     scene.background = backgroundTexture;
 
     // Setup Meme
-    const video = document.getElementById('video');
-    video.load();
     video.play();
-
     const videoTexture = new THREE.VideoTexture(video);
     const videoMaterial =  new THREE.MeshBasicMaterial( {map: videoTexture, side: THREE.FrontSide, toneMapped: false} );
     const videoScreen = new THREE.Mesh(new THREE.BoxGeometry(), videoMaterial);
@@ -78,13 +93,23 @@ function InitiateMeme() {
     const groundColor = 0xB97A20;
     const intensity = 1;
     const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-
     scene.add(light);
+
+    //Add the preloaded models
+    scene.add(characterModel);
+    scene.add(stageModel);
+
+    //Add in camera controls
+    const controls = new OrbitControls( camera, renderer.domElement );
 
     function animate() {
         var delta = clock.getDelta();
 
         videoScreen.rotation.y += 1 * delta;
+
+        camera.position.x += cameraMove.x;
+        camera.position.y += cameraMove.y;
+        controls.update();
 
         renderer.render(scene, camera);
         if(mixer) {
@@ -104,6 +129,51 @@ function InitiateMeme() {
 
     camera.position.z = 600;
     camera.position.y = 150;
+
+    let leftArrow = keyboard("ArrowLeft");
+    let rightArrow = keyboard("ArrowRight");
+    let upArrow = keyboard("ArrowUp");
+    let downArrow = keyboard("ArrowDown");
+
+    leftArrow.press = () => {
+        cameraMove.x -= 5;
+    };
+
+    leftArrow.release = () => {
+        if(cameraMove.x == -5) {
+            cameraMove.x = 0;
+        }
+    };
+
+    rightArrow.press = () => {
+        cameraMove.x = 5;
+    };
+
+    rightArrow.release = () => {
+        if(cameraMove.x == 5) {
+            cameraMove.x = 0;
+        }
+    };
+
+    downArrow.press = () => {
+        cameraMove.y = -5;
+    };
+
+    downArrow.release = () => {
+        if(cameraMove.y == -5) {
+            cameraMove.y = 0;
+        }
+    };
+
+    upArrow.press = () => {
+        cameraMove.y = 5;
+    };
+
+    upArrow.release = () => {
+        if(cameraMove.y == 5) {
+            cameraMove.y = 0;
+        }
+    };
 
     animate();
 }
